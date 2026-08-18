b3fbcdaf21e6309e7c5ed7545b83f98673d48da3
dedinhnguyen <dinhde1221@gmail.com>
2026-08-18 23:21:52 +0700

    feat(cloud): configure supabase database and vercel frontend deployment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,10 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080/api',
+  supabase: {
+    url: 'https://epmqnamfibmgdeoeguqj.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVwbXFuYW1maWJtZ2Rlb2VndXFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcwNjE4OTQsImV4cCI6MjEwMjYzNzg5NH0.CZbeghtBnlWkyzBOcUbTEh-QT_5o1tIRe6IeaFnaZ2k',
+    publishableKey: 'sb_publishable_cn5mLTiJjQdrk38aAXnFsg_WfnkOahj'
+  }
 };
+
